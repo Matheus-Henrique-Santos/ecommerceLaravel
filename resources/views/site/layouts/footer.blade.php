b3ad8ad835b9e15b3db96a50b94472e46b45cbd1
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-11-26 14:32:32 -0300

    att

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="color-gray-text">
         <h1>Funiro.</h1>
         <p>400 University Drive Suite 200</p>
-        <p>Coral Gables,</p>     
+        <p>Coral Gables,</p>
         <p>FL 33134 USA</p>
     </div>
     <div>
