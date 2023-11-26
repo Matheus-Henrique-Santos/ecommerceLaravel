79a38bd117ce7afa596525b8d938c850845167f9
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-11-26 16:03:09 -0300

    Correção

--- resources/views/site/layouts/navbar.blade.php
+++ resources/views/site/layouts/navbar.blade.php
@@ -1,60 +1,4 @@
-
-<header class="container">
-        <div class="page-header">
-            <div class="logo">
-                <a href="img/navbar/logo.svg">Furniro.</a>
-            </div>
-            <input type="checkbox" id="click">
-
-            <label for="click" class="mainicon">
-                <div class="menu">
-                    <i class='bx bx-menu'></i>
-                </div>
-            </label>
-            <ul>
-
-                <li><a href="{{ route('site.index') }}" class="active" style="--navAni:1">Home</a></li>
-
-                <li><a href="{{ route('site.shop') }}" 
-                style="--navAni:2">Shop</a></li>
-
-                <li><a href="{{ route('site.sobre-nos') }} " style="--navAni:3">Sobre</a></li>
-
-                <li><a href="{{ route('site.contato') }}" style="--navAni:4">Contato</a></li>
-
-                <li><a href="#" style="--navAni:5">Dev's</a></li>
-
-            </ul>
-
-            <div class="icons-nav">
-
-        <span>
-            <img src="img/navbar/person.svg" alt="account">
-        </span>
-
-        <span>
-            <img src="img/navbar/favorite.svg" alt="heart">
-        </span>
-
-        <span>
-            <img src="img/navbar/search.svg" alt="search">
-        </span>
-
-        <span>
-            <img src="img/navbar/shopping_cart.svg" alt="shopping">
-        </span>
-        
-    </div>
-            <label class="mode">
-                <input type="checkbox" id="darkModeToggle">
-                <i class='bx bxs-moon'></i>
-            </label>
-
-        </div>
-    </header>
-
-
-<!-- <nav>
+<nav>
 
     <div class="logo">
         <img src="img/navbar/logo.svg">
@@ -76,18 +20,17 @@
                 <a href="{{ route('site.contato') }}">Contato</a>
             </li>
         </ul>
-
     </div>
 
 
     <div class="icons-nav">
 
         <span>
-            <img src="img/navbar/account-alert.svg" alt="account">
+            <img src="img/navbar/person.svg" alt="account">
         </span>
 
         <span>
-            <img src="img/navbar/heart.svg" alt="heart">
+            <img src="img/navbar/favorite.svg" alt="heart">
         </span>
 
         <span>
@@ -95,15 +38,9 @@
         </span>
 
         <span>
-            <img src="img/navbar/shopping-cart.svg" alt="shopping">
+            <img src="img/navbar/shopping_cart.svg" alt="shopping">
         </span>
-        
+
     </div>
 
-            <label class="mode">
-            <input type="checkbox" id="darkModeToggle">
-            <i class='bx bxs-moon'></i>
-            </label>
-
 </nav>
- -->
