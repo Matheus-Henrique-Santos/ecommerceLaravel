c7ca61090aeb1d0d695fd2709caa096e0ded1a74
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-11-26 15:31:30 -0300

    Initial footer

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -1,23 +1,25 @@
 <footer>
     <div class="color-gray-text">
         <h1>Funiro.</h1>
-        <p>400 University Drive Suite 200</p>
-        <p>Coral Gables,</p>     
+        <p>400 University Drive Suite 200 Coral</p>
+        <p>Gables,</p>     
         <p>FL 33134 USA</p>
     </div>
-    <div>
-        <h1>footer aqui</h1>
+    <div class="color-gray-text">
+        <p class="footer-title">Links</p>
+        <p class="options-footer">Home</p>  
+        <p class="options-footer">Shop</p>     
+        <p class="options-footer">About</p>     
+        <p class="options-footer">Contact</p>     
     </div>
-    <div>
-        <h1>footer aqui</h1>
+    <div class="color-gray-text">
+        <p class="footer-title">Help</p>
+        <p class="options-footer">Payment Options</p>  
+        <p class="options-footer">Returns</p>     
+        <p class="options-footer">Privacy Policies</p>         
     </div>
-    <div>
-        <h1>footer aqui</h1>
-    </div>
-    <div>
-        <h1>footer aqui</h1>
-    </div>
-    <div>
-        <h1>footer aqui</h1>
+    <div class="color-gray-text">
+        <p class="footer-title">Newsletter</p>  
+        <input type="text" placeholder="Enter Your Email Address" />
     </div>
 </footer>
