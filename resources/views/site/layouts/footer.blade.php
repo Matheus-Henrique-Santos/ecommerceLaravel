89f5f4f4ac817d7611da2652f26fd53175c9d2d9
Matheus Henrique <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2023-11-26 13:23:57 -0300

    SCSS FOOTER

--- resources/views/site/layouts/footer.blade.php
+++ resources/views/site/layouts/footer.blade.php
@@ -1,4 +1,23 @@
-<div>
-
-    <h1>footer aqui</h1>
-</div>
+<footer>
+    <div class="color-gray-text">
+        <h1>Funiro.</h1>
+        <p>400 University Drive Suite 200</p>
+        <p>Coral Gables,</p>     
+        <p>FL 33134 USA</p>
+    </div>
+    <div>
+        <h1>footer aqui</h1>
+    </div>
+    <div>
+        <h1>footer aqui</h1>
+    </div>
+    <div>
+        <h1>footer aqui</h1>
+    </div>
+    <div>
+        <h1>footer aqui</h1>
+    </div>
+    <div>
+        <h1>footer aqui</h1>
+    </div>
+</footer>
