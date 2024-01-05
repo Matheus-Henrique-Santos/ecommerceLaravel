f4c862712ba4b2fcf95c8e4407f159d2e82fa0ab
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-01-04 21:50:30 -0300

    att

--- resources/views/site/components/product.blade.php
+++ resources/views/site/components/product.blade.php
@@ -11,20 +11,18 @@
     <div class="product-hidden">
         <div class="product-hidden-content">
             <div>
-                <button>Adicionar ao Carrinho</button>
+                <button>ADICIONAR AO CARRINHO</button>
             </div>
             <div class="product-hidden-links">
                 <ul>
                     <li>
-                        <img src="img/main/product/share.svg" alt="share">
-                        <a href="#">Compartilhar</a>
+                        <a href="#"><img src="img/main/product/share.svg" alt="share">Postar</a>
                     </li>
                     <li>
-                        <img src="img/main/product/compare.svg" alt="compare">
-                        <a href="#">Compare</a>
+                        <a href="#"><img src="img/main/product/compare.svg" alt="compare">Compare</a>
                     </li>
                     <li>
-                        <img src="img/main/product/heart.svg" alt="heart"><a href="#">Gostei</a>
+                        <a href="#"><img src="img/main/product/heart.svg" alt="heart">Gostei</a>
                     </li>
                 </ul>
             </div>
